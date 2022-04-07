ff96c641cd33c08284a3e0f28cb5d09f0886de67
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-07 16:13:13 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+import os, time
+
+os.system('clear')
+os.system('pip install colorama')
+os.system('clear')
+
+from colorama import Fore, Style
+
+print ('\n')
+def res():
+    print(Style.RESET_ALL)
+
+
